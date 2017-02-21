7207a7cac3cd4aef8d5f210a718f6609a77d2528
Anson Mansfield <anson.mansfield@gmail.com>
2017-02-21 14:20:19 -0700

    initial

---
+++ TriacDimmer.cpp
@@ -0,0 +1,102 @@
+#include "TriacDimmer.h"
+
+
+void TriacDimmer::begin(){
+	TCCR1A = 0;
+	TCCR1B = _BV(ICNC1) | _BV(ICES1) | _BV(CS11);
+	TIMSK1 = _BV(ICIE1);
+}
+
+void TriacDimmer::setBrightness(uint8_t pin, float x){
+	if(pin == 9){
+		float y = 1 - TriacDimmer::detail::interpolate(x,
+			TriacDimmer::detail::brightness_lut,
+			TriacDimmer::detail::phase_lut,
+			TriacDimmer::detail::lut_length);
+
+		TriacDimmer::detail::ch_A_up = (1-y) * ICR1;
+		TriacDimmer::detail::ch_A_dn = TriacDimmer::detail::ch_A_up + TriacDimmer::detail::pulse_length;
+	} else if(pin == 10){
+		float y = 1 - TriacDimmer::detail::interpolate(x,
+			TriacDimmer::detail::brightness_lut,
+			TriacDimmer::detail::phase_lut,
+			TriacDimmer::detail::lut_length);
+
+		TriacDimmer::detail::ch_B_up = (1-y) * ICR1;
+		TriacDimmer::detail::ch_B_dn = TriacDimmer::detail::ch_B_up + TriacDimmer::detail::pulse_length;
+	}
+}
+
+float TriacDimmer::getCurrentBrightness(uint8_t pin){
+	if(pin == 9){
+		float y = 1 - (float) TriacDimmer::detail::ch_A_up / ICR1;
+		return TriacDimmer::detail::interpolate(y,
+			TriacDimmer::detail::phase_lut,
+			TriacDimmer::detail::brightness_lut,
+			TriacDimmer::detail::lut_length);
+	} else if(pin == 10){
+		float y = 1 - (float) TriacDimmer::detail::ch_B_up / ICR1;
+		return TriacDimmer::detail::interpolate(y,
+			TriacDimmer::detail::phase_lut,
+			TriacDimmer::detail::brightness_lut,
+			TriacDimmer::detail::lut_length);
+	}
+	return 0;
+}
+
+void TriacDimmer::end(){
+	TCCR1A = 0;
+	TCCR1B = 0;
+	TIMSK1 = 0;
+}
+
+const float TriacDimmer::detail::interpolate(const float x, const float x_table[], const float y_table[], uint8_t size){
+	if (x <= x_table[0]) return y_table[0];
+	if (x >= x_table[size-1]) return y_table[size-1];
+	uint8_t pos = 1;
+	while(x > x_table[pos]) pos++;
+	if (x == x_table[pos]) return y_table[pos];
+	return (x - x_table[pos-1]) * (y_table[pos] - y_table[pos-1]) / (x_table[pos] - x_table[pos-1]) + y_table[pos-1];
+}
+
+ISR(TIMER1_CAPT_vect){
+	OCR1A = TriacDimmer::detail::ch_A_up;
+	OCR1B = TriacDimmer::detail::ch_B_up;
+	TCCR1A = _BV(COM1A0) | _BV(COM1A1) | _BV(COM1B0) | _BV(COM1B1);
+	TIMSK1 = _BV(ICIE1) | _BV(OCIE1A) | _BV(OCIE1B);
+
+	// note, this is equivalent to `TCNT1 = TCNT1 - ICR1 + 1;`, 
+	// but written in assembly to make sure the timing is corrent
+	// the "ld r24,Z" and "st Z,r24" must be exactly 8 instructions apart
+	register uint16_t tmpA;
+	register uint16_t tmpB;
+	asm volatile (
+		"lds %A[tmpA],134	; load ICR1L\n\t"\
+		"lds %B[tmpA],134+1	; load ICR1H\n\t"\
+		"ld %A[tmpB],Z	; load TCNT1L\n\t"\
+		"ldd %B[tmpB],Z+1	; load TCNT1H\n\t"\
+		"adiw %A[tmpB],1	; add 1\n\t"\
+		"sub %A[tmpB],%A[tmpA]	; subtract ICR1L\n\t"\
+		"sbc %B[tmpB],%B[tmpA]	; subtract ICR1H\n\t"\
+		"nop	; pad for timing\n\t"\
+		"std Z+1,%B[tmpB]	; store TCNT1H\n\t"\
+		"st Z,%A[tmpB]	; store TCNT1L\n\t"\
+	: [tmpA] "=&r" (tmpA), [tmpB] "=&r" (tmpB) );
+}
+ISR(TIMER1_COMPA_vect){
+	TCCR1A &=~ _BV(COM1A1);
+	TIMSK1 &=~ _BV(OCIE1A);
+	OCR1A = TriacDimmer::detail::ch_A_dn;
+	if(TCNT1 - OCR1A >= 0){
+		TCCR1C = _BV(FOC1A);
+	}
+}
+ISR(TIMER1_COMPB_vect){
+	TCCR1B &=~ _BV(COM1B1);
+	TIMSK1 &=~ _BV(OCIE1B);
+	OCR1B = TriacDimmer::detail::ch_B_dn;
+	if(TCNT1 - OCR1B >= 0){
+		TCCR1C = _BV(FOC1B);
+	}
+}
+
---
+++ TriacDimmer.h
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+#include <avr/pgmspace.h>
+
+
+
+namespace TriacDimmer {
+	void begin();
+	void setBrightness(uint8_t pin, float value);
+	float getCurrentBrightness(uint8_t pin);
+	void end();
+
+	namespace detail {
+
+		const uint16_t pulse_length = 20;
+		volatile uint16_t ch_A_up = 0;
+		volatile uint16_t ch_A_dn = 0;
+		volatile uint16_t ch_B_up = 0;
+		volatile uint16_t ch_B_dn = 0;
+
+		const PROGMEM float phase_lut[] = 
+		{ -0.            ,   9.25295293e-08,   2.52559560e-05,   9.66419104e-04,
+		   6.17766236e-03,   1.89552895e-02,   4.10991260e-02,   7.40639247e-02,
+		   1.15521833e-01,   1.64535545e-01,   2.20903015e-01,   2.80866796e-01,
+		   3.43594021e-01,   4.10268098e-01,   4.77649597e-01,   5.43711948e-01,
+		   6.07640486e-01,   6.68898386e-01,   7.26232887e-01,   7.78168688e-01,
+		   8.24150458e-01,   8.64133390e-01,   8.98217770e-01,   9.26632476e-01,
+		   9.49644260e-01,   9.67583008e-01,   9.80871218e-01,   9.90041286e-01,
+		   9.95740338e-01,   9.98723761e-01,   9.99838777e-01,   1.00000000e+00};
+		const PROGMEM float  brightness_lut[] =
+		{ 0.        ,  0.03225806,  0.06451613,  0.09677419,  0.12903226,
+		  0.16129032,  0.19354839,  0.22580645,  0.25806452,  0.29032258,
+		  0.32258065,  0.35483871,  0.38709677,  0.41935484,  0.4516129 ,
+		  0.48387097,  0.51612903,  0.5483871 ,  0.58064516,  0.61290323,
+		  0.64516129,  0.67741935,  0.70967742,  0.74193548,  0.77419355,
+		  0.80645161,  0.83870968,  0.87096774,  0.90322581,  0.93548387,
+		  0.96774194,  1.        };
+		const uint8_t lut_length = sizeof(phase_lut) / sizeof(float);
+
+		const float interpolate(const float x,
+			const float x_table[],
+			const float y_table[],
+			uint8_t table_length);
+	}
+};
+
+ISR(TIMER1_CAPT_vect);
+ISR(TIMER1_COMPA_vect);
+ISR(TIMER1_COMPB_vect);
